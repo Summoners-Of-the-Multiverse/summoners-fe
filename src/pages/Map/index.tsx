ecccdde302db39abb0ce89e22d8c8b1e61111b22
kida <leslug.away@gmail.com>
2022-10-01 22:01:31 +0800

    Updated starter map

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useContext } from 'react';
+import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 import { AddressContext } from '../../App';
 import instance from '../Axios';
@@ -26,6 +27,7 @@ const travel = async(address: string, areaId: number) => {
 
 const Map = ({ onAreaChange }: MapProps) => {
     const { address, areaId } = useContext(AddressContext);
+    const navigate = useNavigate();
 
     const onTravelClick = useCallback(async (areaId: number) => {
         let hasTravelled = await travel(address, areaId);
@@ -35,31 +37,36 @@ const Map = ({ onAreaChange }: MapProps) => {
         }
     }, [address, onAreaChange]);
 
+    const onCloseClick = useCallback(() => {
+        navigate(-1);
+    }, [navigate]);
+
     return (
 		<div className='map-page'>
             <div className="map-container">
-			    <img className='map' src="/assets/maps/starter_map.jpeg" alt="starter_map" />
-                <button className={`map-button ${areaId === 1? 'active' : ''}`} style={{ bottom: '22%', left: '13.8%' }} onClick={() => { onTravelClick(1) }}>
+			    <img className='map' src="/assets/maps/starter_map.jpg" alt="starter_map" />
+                <button className={`map-button ${areaId === 1? 'active' : ''}`} style={{ bottom: '10%', left: '10%' }} onClick={() => { onTravelClick(1) }}>
                     <i className="fa fa-map-marker"></i>
                 </button>
-                <button className={`map-button ${areaId === 2? 'active' : ''}`} style={{ bottom: '24%', left: '63.2%' }} onClick={() => { onTravelClick(2) }}>
+                <button className={`map-button ${areaId === 2? 'active' : ''}`} style={{ bottom: '19%', left: '30%' }} onClick={() => { onTravelClick(7) }}>
                     <i className="fa fa-map-marker"></i>
                 </button>
-                <button className={`map-button ${areaId === 3? 'active' : ''}`} style={{ bottom: '52%', left: '41%' }} onClick={() => { onTravelClick(3) }}>
+                <button className={`map-button ${areaId === 3? 'active' : ''}`} style={{ bottom: '24%', left: '54%' }} onClick={() => { onTravelClick(2) }}>
                     <i className="fa fa-map-marker"></i>
                 </button>
-                <button className={`map-button ${areaId === 4? 'active' : ''}`} style={{ bottom: '55%', left: '64%' }} onClick={() => { onTravelClick(4) }}>
+                <button className={`map-button ${areaId === 4? 'active' : ''}`} style={{ bottom: '52%', left: '30%' }} onClick={() => { onTravelClick(3) }}>
                     <i className="fa fa-map-marker"></i>
                 </button>
-                <button className={`map-button ${areaId === 5? 'active' : ''}`} style={{ bottom: '70%', left: '64%' }} onClick={() => { onTravelClick(5) }}>
+                <button className={`map-button ${areaId === 5? 'active' : ''}`} style={{ bottom: '55%', left: '68%' }} onClick={() => { onTravelClick(4) }}>
                     <i className="fa fa-map-marker"></i>
                 </button>
-                <button className={`map-button ${areaId === 6? 'active' : ''}`} style={{ bottom: '80%', left: '16%' }} onClick={() => { onTravelClick(6) }}>
+                <button className={`map-button ${areaId === 6? 'active' : ''}`} style={{ bottom: '73%', left: '70%' }} onClick={() => { onTravelClick(5) }}>
                     <i className="fa fa-map-marker"></i>
                 </button>
-                <button className={`map-button ${areaId === 7? 'active' : ''}`} style={{ bottom: '85%', left: '64%' }} onClick={() => { onTravelClick(7) }}>
+                <button className={`map-button ${areaId === 7? 'active' : ''}`} style={{ bottom: '80%', left: '16%' }} onClick={() => { onTravelClick(6) }}>
                     <i className="fa fa-map-marker"></i>
                 </button>
+                <button className='close-button' onClick={onCloseClick}><i className="fa fa-times"></i></button>
             </div>
 		</div>
 	)
