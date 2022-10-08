19260a3a6f8a5f054f5748a9048c30ad690dfa46
kida <leslug.away@gmail.com>
2022-10-09 01:26:53 +0800

    Removed redundant script

--- src/pages/Inventory/index.tsx
+++ src/pages/Inventory/index.tsx
@@ -71,20 +71,6 @@ const Inventory = () => {
     const take = 15;
     const maxEquipped = 4;
 
-    // load popover
-    useEffect(() => {
-        const script = document.createElement('script');
-
-        script.src = "https://use.typekit.net/foobar.js";
-        script.async = true;
-
-        document.body.appendChild(script);
-
-        return () => {
-          document.body.removeChild(script);
-        }
-    }, []);
-
     useEffect(() => {
         const getInventory = async(chain: string, address: string) => {
             if (address && chain) {
